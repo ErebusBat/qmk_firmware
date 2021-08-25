16bea33c87b1930c267b6ab12ba131b7a028a703
Andrew Burns <andrew@fetlife.com>
2021-08-25 08:54:58 -0600

    launch: Add macro/mapping to support screenshots on mac
    Previously `Fn+TAB` was mapped to `PRINT_SCREEN` which is handled in GNOME to launch flameshot.
    
    With new setup Thelio/GNOME is running Synergy host with a Mac as the primary screen / client.
    
    This PR remaps `Fn+TAB` => `Shift+Cmd+5` which will launch Skitch on the mac.
    
    Because we still want to take screenshots on GNOME occasionally we can now do that with `Fn+\`` (Function + Tilde)

--- keyboards/system76/launch_1/keymaps/erebusbat/keymap.c
+++ keyboards/system76/launch_1/keymaps/erebusbat/keymap.c
@@ -1,5 +1,11 @@
 #include QMK_KEYBOARD_H
 
+// https://beta.docs.qmk.fm/using-qmk/advanced-keycodes/feature_macros
+// see process_record_user below for handling of these custom keycodes
+enum custom_keycodes {
+  MAC_SS = SAFE_RANGE,
+};
+
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
   /* Layer 0, default layer
@@ -60,8 +66,8 @@ ________________________________________________________________________________
 
   [1] = LAYOUT(
     RESET,   KC_TRNS, KC_TRNS,  KC_F13,  KC_F14,  KC_F15,  KC_F16,  KC_F17,  KC_F18,  KC_F19,  KC_F20,  KC_F21,  KC_F22, KC_MEDIA_PREV_TRACK, KC_MPLY,
-    KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, RGB_TOG, RGB_VAD, RGB_VAI, KC_MEDIA_NEXT_TRACK, KC_VOLU,
-        KC_PSCR, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_HOME, KC_PGDN, KC_PGUP, KC_END,  KC_TRNS, KC_TRNS, KC_TRNS,         KC_TRNS, KC_VOLD,
+    KC_PSCR, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, RGB_TOG, RGB_VAD, RGB_VAI, KC_MEDIA_NEXT_TRACK, KC_VOLU,
+        MAC_SS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_HOME, KC_PGDN, KC_PGUP, KC_END,  KC_TRNS, KC_TRNS, KC_TRNS,         KC_TRNS, KC_VOLD,
           KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, KC_TRNS, KC_TRNS,         LALT(KC_ENT),   KC_MUTE,
               KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, RCS(KC_V), KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,   KC_TRNS,        KC_PGUP,
           KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,     KC_TRNS,          KC_TRNS,      KC_TRNS, KC_TRNS, KC_TRNS,                KC_HOME, KC_PGDN, KC_END
@@ -87,5 +93,13 @@ ________________________________________________________________________________
 };
 
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
-    return true;
+  switch (keycode) {
+    case MAC_SS:
+      if (record->event.pressed) {
+        SEND_STRING(SS_DOWN(X_LSFT) SS_LGUI("5") SS_UP(X_LSFT));
+      }
+      break;
+  }
+  return true;
+
 }
