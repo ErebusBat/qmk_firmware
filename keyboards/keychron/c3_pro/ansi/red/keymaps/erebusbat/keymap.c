dafc0cd7bb7098f5f1ce0d046b97bdabccabf741
Andrew Burns <andrew.burns@companycam.com>
2025-07-28 11:56:33 -0500

    Create custom screenshot macro

--- keyboards/keychron/c3_pro/ansi/red/keymaps/erebusbat/keymap.c
+++ keyboards/keychron/c3_pro/ansi/red/keymaps/erebusbat/keymap.c
@@ -24,13 +24,17 @@ enum layers{
     WIN_FN,
 };
 
+enum custom_keycodes {
+    EB_SCRN = SAFE_RANGE
+};
+
 #define KC_TASK LGUI(KC_TAB)
 #define KC_FLXP LGUI(KC_E)
 
 // clang-format off
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [MAC_BASE] = LAYOUT_tkl_ansi(
-        EB_ESC_HYPR,               KC_F1,       KC_F2,    KC_F3,    KC_F4,    KC_F5,    KC_F6,    KC_F7,    KC_F8,    KC_F9,    KC_F10,   KC_F11,     KC_F12,   KC_SCRN,  KC_SCRL,  BL_STEP,
+        EB_ESC_HYPR,               KC_F1,       KC_F2,    KC_F3,    KC_F4,    KC_F5,    KC_F6,    KC_F7,    KC_F8,    KC_F9,    KC_F10,   KC_F11,     KC_F12,   EB_SCRN,  KC_SCRL,  BL_STEP,
         KC_GRV,      KC_1,         KC_2,        KC_3,     KC_4,     KC_5,     KC_6,     KC_7,     KC_8,     KC_9,     KC_0,     KC_MINS,  KC_EQL,     KC_BSPC,  KC_INS,   KC_HOME,  KC_PGUP,
         KC_TAB,      KC_Q,         KC_W,        KC_E,     KC_R,     KC_T,     KC_Y,     KC_U,     KC_I,     KC_O,     KC_P,     KC_LBRC,  KC_RBRC,    KC_BSLS,  KC_DEL,   KC_END,   KC_PGDN,
         EB_CAPS,     KC_A,         KC_S,        KC_D,     KC_F,     KC_G,     KC_H,     KC_J,     KC_K,     KC_L,     KC_SCLN,  KC_QUOT,              KC_ENT,
@@ -61,6 +65,17 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         _______,            _______,  _______,  _______,  _______,  _______,  NK_TOGG,  _______,  _______,  _______,  _______,              _______,            _______,
         _______,  _______,  _______,                                _______,                                _______,  _______,  _______,    _______,  _______,  _______,  _______),
 };
+
+bool process_record_user(uint16_t keycode, keyrecord_t *record) {
+    switch (keycode) {
+        case EB_SCRN:
+            if (record->event.pressed) {
+                SEND_STRING(SS_LGUI(SS_LSFT("4")));
+            }
+            return false;
+    }
+    return true;
+}
 
 void keyboard_post_init_user(void) {
     // Force MAC_BASE as default layer
