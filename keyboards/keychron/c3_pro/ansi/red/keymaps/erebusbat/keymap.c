77997d880cc7991bb52ff7514dc24e10e07179b3
Andrew Burns <ErebusBat@gmail.com>
2026-03-01 14:17:23 -0700

    c3_pro: Add AI_NXT key

--- keyboards/keychron/c3_pro/ansi/red/keymaps/erebusbat/keymap.c
+++ keyboards/keychron/c3_pro/ansi/red/keymaps/erebusbat/keymap.c
@@ -30,20 +30,20 @@ enum layers {
 // clang-format off
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [MAC_BASE] = LAYOUT_tkl_ansi(
-        // ESC,                 KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_F5,    KC_F6,    KC_F7,    KC_F8,    KC_F9,     KC_F10,     KC_F11,     KC_F11,   KC_PSCR,    KC_SCRL,  KC_PAUS,
-        EB_ESC_HYPR,            KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_F5,    KC_F6,    KC_F7,    KC_F8,    KC_F9,     KC_F10,     KC_F11,     KC_F12,   EB_SCRN,    KC_SCRL,  EB_DICT,
+        // ESC,                 KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_F5,    KC_F6,    KC_F7,    KC_F8,    KC_F9,     KC_F10,     KC_F11,     KC_F11,   KC_PSCR,    KC_SCRL,   KC_PAUS,
+        EB_ESC_HYPR,            KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_F5,    KC_F6,    KC_F7,    KC_F8,    KC_F9,     KC_F10,     KC_F11,     KC_F12,   EB_SCRN,    EB_AI_NXT, EB_DICT,
 
-        // GRV,       KC_1,     KC_2,     KC_3,     KC_4,     KC_5,     KC_6,     KC_7,     KC_8,     KC_9,     KC_0,      KC_MINS,    KC_EQL,     KC_BSPC,  KC_INS,     KC_HOME,  KC_PGUP,
-        KC_GRV,       KC_1,     KC_2,     KC_3,     KC_4,     KC_5,     KC_6,     KC_7,     KC_8,     KC_9,     KC_0,      KC_MINS,    KC_EQL,     KC_BSPC,  KC_INS,     KC_HOME,  KC_PGUP,
+        // GRV,       KC_1,     KC_2,     KC_3,     KC_4,     KC_5,     KC_6,     KC_7,     KC_8,     KC_9,     KC_0,      KC_MINS,    KC_EQL,     KC_BSPC,  KC_INS,     KC_HOME,   KC_PGUP,
+        KC_GRV,       KC_1,     KC_2,     KC_3,     KC_4,     KC_5,     KC_6,     KC_7,     KC_8,     KC_9,     KC_0,      KC_MINS,    KC_EQL,     KC_BSPC,  KC_INS,     KC_HOME,   KC_PGUP,
 
-        // TAB,       KC_Q,     KC_W,     KC_E,     KC_R,     KC_T,     KC_Y,     KC_U,     KC_I,     KC_O,     KC_P,      KC_LBRC,    KC_RBRC,    KC_BSLS,  KC_DEL,     KC_END,   KC_PGDN,
-        KC_TAB,       KC_Q,     KC_W,     KC_E,     KC_R,     KC_T,     KC_Y,     KC_U,     KC_I,     KC_O,     KC_P,      KC_LBRC,    KC_RBRC,    KC_BSLS,  KC_DEL,     KC_END,   KC_PGDN,
+        // TAB,       KC_Q,     KC_W,     KC_E,     KC_R,     KC_T,     KC_Y,     KC_U,     KC_I,     KC_O,     KC_P,      KC_LBRC,    KC_RBRC,    KC_BSLS,  KC_DEL,     KC_END,    KC_PGDN,
+        KC_TAB,       KC_Q,     KC_W,     KC_E,     KC_R,     KC_T,     KC_Y,     KC_U,     KC_I,     KC_O,     KC_P,      KC_LBRC,    KC_RBRC,    KC_BSLS,  KC_DEL,     KC_END,    KC_PGDN,
 
         // CAPS,      KC_A,     KC_S,     KC_D,     KC_F,     KC_G,     KC_H,     KC_J,     KC_K,     KC_L,     KC_SCLN,   KC_QUOT,                KC_ENT,
         EB_CAPS,      KC_A,     KC_S,     KC_D,     KC_F,     KC_G,     KC_H,     KC_J,     KC_K,     KC_L,     KC_SCLN,   KC_QUOT,                KC_ENT,
 
-        // LSFT,                KC_Z,     KC_X,     KC_C,     KC_V,     KC_B,     KC_N,     KC_M,     KC_COMM,  KC_DOT,    KC_SLSH,                KC_RSFT,              KC_UP,
-        KC_LSFT,                KC_Z,     KC_X,     KC_C,     KC_V,     KC_B,     KC_N,     KC_M,     KC_COMM,  KC_DOT,    KC_SLSH,                KC_RSFT,              KC_UP,
+        // LSFT,                KC_Z,     KC_X,     KC_C,     KC_V,     KC_B,     KC_N,     KC_M,     KC_COMM,  KC_DOT,    KC_SLSH,                KC_RSFT,                KC_UP,
+        KC_LSFT,                KC_Z,     KC_X,     KC_C,     KC_V,     KC_B,     KC_N,     KC_M,     KC_COMM,  KC_DOT,    KC_SLSH,                KC_RSFT,                KC_UP,
 
         // LCTL,      KC_LWIN,  KC_LALT,                                KC_SPC,                                 KC_RALT,   MO(WIN_FN), KC_APP,     KC_RCTL,  KC_LEFT,     KC_DOWN,  KC_RGHT),
         MO(MAC_FN),   KC_LOPT,  KC_LCMD,                                KC_SPC,                                 KC_RCMD,   MO(MAC_FN), KC_TRNS,    KC_RCTL,  KC_LEFT,     KC_DOWN,  KC_RGHT),
@@ -59,7 +59,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         BL_TOGG,      BL_STEP,  BL_UP,       _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,    _______,  _______,  _______,  _______,
 
         // CAPS,      KC_A,     KC_S,        KC_D,     KC_F,     KC_G,     KC_H,     KC_J,     KC_K,     KC_L,     KC_SCLN,  KC_QUOT,              KC_ENT,
-        MO(WIN_BASE), _______,  BL_DOWN,     _______,  _______,  _______,  KC_LEFT,  KC_DOWN,  KC_UP,    KC_RGHT,  _______,  _______,              _______,
+        MO(WIN_BASE), _______,  BL_DOWN,     _______,  _______,  _______,  KC_LEFT,  KC_DOWN,  KC_UP,    KC_RGHT,  _______,  _______,              EB_TUI_NL,
 
         // LSFT,                KC_Z,        KC_X,     KC_C,     KC_V,     KC_B,     KC_N,     KC_M,     KC_COMM,  KC_DOT,   KC_SLSH,              KC_RSFT,            KC_UP,
         C(KC_LSFT),             C(KC_Z),     C(KC_X),  C(KC_C),  C(KC_V),  _______,  NK_TOGG,  _______,  _______,  _______,  _______,              _______,            _______,
