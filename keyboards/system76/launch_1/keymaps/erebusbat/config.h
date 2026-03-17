b899a673900b036a756b2d61c2e946fb206466ca
Andrew Burns <andrew.burns@lightspeedhq.com>
2026-03-17 14:33:20 -0600

    sys76: Improve Launch 1 chording and WM media keys

---
+++ keyboards/system76/launch_1/keymaps/erebusbat/config.h
@@ -0,0 +1,14 @@
+/* Copyright 2026 @ ErebusBat
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ */
+
+#pragma once
+
+// Match K2 HE mod-tap behavior for faster chord recognition.
+#define TAPPING_TERM 200
+#define PERMISSIVE_HOLD
+#define HOLD_ON_OTHER_KEY_PRESS
